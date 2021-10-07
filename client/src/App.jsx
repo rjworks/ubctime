eb8442a193432c2d3d2364def78436a446abfd4f
rjworks <arjayandal05@yahoo.com>
2021-10-06 19:28:17 -0700

    add /api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import Home from "./components/Home/Home";
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './styles.css';
+
 const App = () => (
-    window.innerWidth <= 760 ? <div>Sorry, this website is currently only supported on larger screens.
-        Check back later for updates!</div> :
-        <Home/>
+    <div>
+            {window.innerWidth <= 760 ? <div>Sorry, this website is currently only supported on larger screens.
+                    Check back later for updates!</div> : <Home/>}
+            <Router>
+                    <Switch>
+                            <Route exact
+                                   path="/api"
+                                   render={() =>
+                                       (window.location = "https://github.com/cupof2/ubctime-api")}
+                            />
+                    </Switch>
+            </Router>
+    </div>
 );
 
 export default App;
